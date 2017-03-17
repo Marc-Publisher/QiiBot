557472b9105c572b55245f88b18b3fdbf81c126b
Marc-Publisher <spamspamm8@gmail.com>
2017-03-17 19:16:01 +0000

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,22 @@
+window.onload = (function(){
+	var url = window.location.href;
+	var urlArray = url.split("/")
+	var len = urlArray.length;
+	var id = parseInt((urlArray[len-1]).replace("#",""));
+	var nextURL = "http://qiibee.com/app/feed/"+(id-1).toString()+"#";
+	var selector = "a.post-like-btn";
+	setTimeout(function(){
+		//CLICK
+		function like(){
+			var a = $(selector);
+			a[0].click();
+			setTimeout(function(){window.location=nextURL;},1000);
+		}
+		function checkForExists(){
+			var ex = $(selector).length != 0;
+			if(ex)like();
+			else setTimeout(checkForExists, 700)
+		}
+		checkForExists();
+	},3000);
+});
